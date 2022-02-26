705423062d2cfeb54455810d26c482c40cb082b8
SlavikusVOG <slavaolish@gmail.com>
2022-02-27 02:35:40 +0300

    fix: fixed function

--- src/index.js
+++ src/index.js
@@ -41,12 +41,12 @@ function decode(expr) {
     const result = [];
     for(let i = 0; i < expr.length; i += 10) {
         const letterMorseArray = [];
-        const binLetter = expr.slice(i, 10);
+        const binLetter = expr.slice(i, i+10);
         if (binLetter === "**********") result.push(" ");
         else {
             for(j = 0; j < 10; j += 2) {
-                if (binLetter.slice(i, 2) === "10") letterMorseArray.push(".");
-                else if (binLetter.slice(i, 2) === "11") letterMorseArray.push("-");
+                if (binLetter.slice(j, j+2) === "10") letterMorseArray.push(".");
+                else if (binLetter.slice(j, j+2) === "11") letterMorseArray.push("-");
             }
             result.push(MORSE_TABLE[letterMorseArray.join("")]);
         }
